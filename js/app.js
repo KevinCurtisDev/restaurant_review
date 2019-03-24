1479c8521f2a7481d0616541660d20c9da0e02d2
kevin <kpcurtis@gmail.com>
2019-03-24 21:14:44 +0000

    set up local json file and retrieved data into app.js  with fetch api

--- js/app.js
+++ js/app.js
@@ -1,64 +1,87 @@
-//get the html node that will contain the javascript generated html
+//Get user's current location
+navigator.geolocation.getCurrentPosition(function (location) {
+    //set variable for user's coordinates
+    var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+
+    //set leafletJS map view to user's current position
+    var mymap = L.map('leafletMap').setView(latlng, 13)
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1Ijoia2V2aW4xOTgxIiwiYSI6ImNqdG16emNoMDJnaTAzeXJyNjNyaXVmYWkifQ.h4o3OiiEqYEzarChFx7-8Q'
+    }).addTo(mymap);
+
+    L.marker(latlng).addTo(mymap)
+    //add a popupto the map marker
+        .bindPopup(`${restaurantReviews.reviews[0].restaurantName} <br> ${restaurantReviews.reviews[0].address}`);
+
+    mymap.addEventListener("click", function (e) {
+        var mp = new L.Marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+        alert(mp.getLatLng());
+    });
+});
+
+//Get restaurant list container from the dom
 const restaurantsList = document.querySelector("#restaurant-list");
+//Get add rating button from the dom
+const addRating = document.getElementById("add-restaurant");
 
-//list of restaurants and reviews
-let reviews = [
-    {
-        "restaurantName": "Bronco",
-        "address": "39 Rue des Petites Écuries, 75010 Paris",
-        "lat": 48.8737815,
-        "long": 2.3501649,
-        "ratings": [
-            {
-                "stars": 4,
-                "comment": "Great! But not many veggie options."
-            },
-            {
-                "stars": 5,
-                "comment": "My favorite restaurant!"
-            }
-        ]
-    },
-    {
-        "restaurantName": "Babalou",
-        "address": "4 Rue Lamarck, 75018 Paris",
-        "lat": 48.8865035,
-        "long": 2.3442197,
-        "ratings": [
-            {
-                "stars": 5,
-                "comment": "Tiny pizzeria next to Sacre Coeur!"
-            },
-            {
-                "stars": 3,
-                "comment": "Meh, it was fine."
-            }
-        ]
+
+addRating.addEventListener("click", (e) => {
+    e.preventDefault();
+    //empty the list of restaurnts before rebuilding the list in the dom
+    restaurantsList.innerHTML = "";
+
+    const addComment = document.getElementById("restaurant-comment").value;
+    const addRating = document.getElementById("restaurant-rating").value;
+    let newReview = {};
+    newReview["stars"] = addRating;
+    newReview["comment"] = addComment;
+
+    reviews[0].ratings.push(newReview);
+    populateRestaurantHtml();
+});
+
+//use fetch api to retrieve restaurant data
+fetch("/data/data.json")
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+        //check that we're getting the correct response
+        console.log(data.reviews[0].restaurantName);
+     
+//function that creates and adds restaurant data to the dom
+const populateRestaurantHtml = () => {
+    for (let i = 0; i <= data.reviews.length; i++) {
+        let restaurantDetails = document.createElement("li");
+        restaurantDetails.classList.add("review");
+
+        let restaurantTitle = document.createElement("p");
+        restaurantTitle.innerHTML = data.reviews[i].restaurantName;
+
+        let restaurantAddress = document.createElement("p");
+        restaurantAddress.innerHTML = data.reviews[i].address;
+
+        restaurantTitle.append(restaurantAddress);
+        restaurantDetails.append(restaurantTitle);
+        restaurantsList.append(restaurantDetails);
+
+        for (let j = 0; j <= data.reviews[i].ratings.length - 1; j++) {
+            let restaurantReview = document.createElement("p");
+            let restaurantStars = document.createElement("p");
+            restaurantStars.innerHTML = `Rating: ${data.reviews[i].ratings[j].stars}`;
+            restaurantReview.innerHTML = `Review: ${data.reviews[i].ratings[j].comment}`;
+            restaurantAddress.append(restaurantReview);
+            restaurantReview.append(restaurantStars);
+        }
+
     }
-]
-
-// create the restaurant list html
-//loop through the review array and create a list item block for each object
-for (let i = 0; i <= reviews.length; i++) {
-    let restaurantDetails = document.createElement("li");
-    restaurantDetails.classList.add("review");
-
-    let restaurantTitle = document.createElement("p");
-    restaurantTitle.innerHTML = reviews[i].restaurantName;
-
-    let restaurantAddress = document.createElement("p");
-    restaurantAddress.innerHTML = reviews[i].address;
-
-    restaurantTitle.append(restaurantAddress);
-    restaurantDetails.append(restaurantTitle);
-    restaurantsList.append(restaurantDetails);
-
-//loop through the reviews of each restaurant object and create the corresponding html
-    for (let j = 0; j <= reviews[i].ratings.length - 1; j++) {
-        console.log(reviews[i].ratings.length);
-        let restaurantReview = document.createElement("p");
-        restaurantReview.innerHTML = `Rating: ${reviews[i].ratings[j].stars} Review: ${reviews[i].ratings[j].comment}`;
-        restaurantAddress.append(restaurantReview);
-    }
-
 }
+
+populateRestaurantHtml();
+
+});  
+
+
+
+
